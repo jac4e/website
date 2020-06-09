cb601045da88280a5db7ae6ca89034a3df773196
Jacques Fourie <jacques@fourie.ca>
2020-06-09 10:50:34 -0600

    input component mostly finished

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+  output: string;
+  width: number;
+  horzPosition: number;
+  blinkInterval: number | undefined;
+  cursorElement: HTMLElement;
+
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.output = 'test';
+    this.width = 21;
+    this.horzPosition = -this.width;
+    this.cursorBlink('start');
+  }
+  cursorBlink(state: string){
+    const cursor = document.querySelector('#cursor') as HTMLElement;
+    function blink(): void{
+      cursor.style.opacity = Math.abs(+cursor.style.opacity - 1).toString();
+    }
+    if (state === 'stop'){
+      window.clearInterval(this.blinkInterval);
+      cursor.style.opacity = (0).toString();
+    } else if (state === 'start'){
+      cursor.style.opacity = (1).toString();
+      this.blinkInterval = window.setInterval(blink, 1100);
+    }
+  }
+  onInput(e){
+    this.updateCursorPos(e);
+    this.output = e.target.value;
+  }
+  updateCursorPos(e){
+    this.cursorBlink('stop');
+    if (e.target.selectionStart <= 21){
+      this.horzPosition = e.target.selectionStart - 21;
+    } else if (e.target.selectionStart > 21){
+      this.horzPosition = 0;
+    }
+    this.cursorBlink('start');
+  }
+  onClick(e){
+    this.updateCursorPos(e);
+    document.querySelector('#input').firstChild.focus();
+  }
+  focus(isFocused: boolean){
+    const cursor = document.querySelector('#cursor') as HTMLElement;
+    if (isFocused){
+       cursor.innerText = '▮';
+    } else {
+      cursor.innerText = '▯';
+    }
+  }
+}
