4e005a6d9313ea223d9bcc090691f2f0bc9eb5e6
Jacques Fourie <jacques@fourie.ca>
2020-06-10 17:46:54 -0600

    Finished input and started fleshing out the rest

---
+++ src/app/console.ts
@@ -0,0 +1,59 @@
+// Search's an array by key and string then returns all objects whose key matches the string
+// Useful for matching path and commands to the proper object.
+function searchArray(arr: object[], key: string, str: string, all: boolean): any {
+    const results = arr.filter( (e) => {
+      const dataToCheck = e[key];
+      if (dataToCheck === undefined){
+        console.log('Array or key does not exist');
+        return;
+      }
+      if (typeof dataToCheck === 'string'){
+        return dataToCheck.toLowerCase().includes(str.toLowerCase());
+      } else {
+        return dataToCheck.some( (o) => o.toLowerCase().includes(str.toLowerCase()));
+      }
+    });
+    return results;
+}
+
+export const DIRECTORY = [
+  {
+    path: '',
+    location: ''
+  },
+];
+
+// Array of commands
+// Each command is stored as an object with th properties "command, usage, desc, and func"
+// Each func should return output for the console, either simple one line outputs to entire files to be served under the header
+
+export const COMMANDS = [
+    {
+        command: 'cd',
+        usage: 'cd [path]',
+        desc: 'changes current directory to [path]',
+        func: (args: string[]) => {
+          if (args.length > 1){
+            // Print invalid arguments
+          } else if (args[0] === '-h') {
+            // Print description
+          }
+        }
+    },
+    {
+        command: 'help',
+        usage: 'help [command]',
+        desc: 'returns a list of all available commands when a command is not specified, else returns the usage and description of the provided command',
+        func: (args: string[]) => {
+            if (args.length === 0){
+              // Print all command descriptions as list
+            } else if (args.length === 1) {
+              // Search command array for args[0]
+              // Print command description if command is found
+            } else {
+              // Print invalid arguments
+            }
+            return;
+        }
+    },
+];
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -8,49 +8,128 @@ import { Component, OnInit } from '@angular/core';
 export class InputComponent implements OnInit {
   output: string;
   width: number;
-  horzPosition: number;
+  cursorPosition: number;
   blinkInterval: number | undefined;
   cursorElement: HTMLElement;
+  history: string[];
 
   constructor() {
   }
   ngOnInit(): void {
-    this.output = 'test';
+    this.output = 'test<br>';
     this.width = 21;
-    this.horzPosition = -this.width;
+    this.cursorPosition = -this.width;
   }
-  cursorBlink(state: string){
+  cursorBlink(state: string) {
     const cursor = document.querySelector('#cursor') as HTMLElement;
-    function blink(): void{
+    function blink(): void {
       cursor.style.opacity = Math.abs(+cursor.style.opacity - 1).toString();
     }
-    if (state === 'stop'){
+    if (state === 'stop') {
       window.clearInterval(this.blinkInterval);
       cursor.style.opacity = (1).toString();
-    } else if (state === 'start'){
+    } else if (state === 'start') {
       this.blinkInterval = window.setInterval(blink, 1100);
     }
   }
-  onInput(e){
-    this.updateCursorPos(e);
-    this.output = e.target.value;
+  onInput(e) {
+    e.preventDefault();
+    console.log(e.key);
+    let selectionStart = e.target.selectionStart;
+    let selectionEnd = e.target.selectionEnd;
+    switch (e.keyCode) {
+      case 8: {// Backspace
+        if (selectionStart !== selectionEnd) {
+          e.target.value = e.target.value.slice(0, selectionStart) + e.target.value.slice(selectionEnd);
+          selectionEnd = selectionStart;
+        } else if (selectionStart !== 0) {
+          e.target.value = e.target.value.slice(0, selectionStart - 1) + e.target.value.slice(selectionEnd);
+          selectionStart -= 1;
+          selectionEnd -= 1;
+        }
+        break;
+      }
+      case 13: {// Enter
+        this.output += e.target.value;
+        e.target.value = '';
+        selectionStart = 0;
+        break;
+      }
+      case 37: {// Arrow left
+        if (e.shiftKey){
+          selectionStart -= 1;
+        } else if (selectionStart !== selectionEnd){
+          selectionEnd = selectionStart;
+        } else {
+          selectionStart -= 1;
+          selectionEnd -= 1;
+        }
+        break;
+      }
+      case 38: {// Arrow up
+        break;
+      }
+      case 39: {// Arrow right
+        if (e.shiftKey){
+          selectionEnd += 1;
+        } else if (selectionStart !== selectionEnd){
+          selectionStart = selectionEnd;
+        } else {
+          selectionStart += 1;
+          selectionEnd += 1;
+        }
+        break;
+      }
+      case 40: {// Arrow down
+        break;
+      }
+      default:
+        // Handle all character keys
+        if (e.key.length === 1 && e.target.value.length < this.width) {
+          if (selectionStart === selectionEnd) {
+            e.target.value = e.target.value.slice(0, selectionStart) + e.key + e.target.value.slice(selectionStart);
+            selectionStart += 1;
+            selectionEnd += 1;
+          } else {
+            e.target.value = e.target.value.replace(e.target.value.slice(selectionStart, selectionEnd), e.key);
+            selectionStart += 1;
+            selectionEnd = selectionStart;
+          }
+        }
+    }
+    // Selection position verification
+    if (selectionStart > e.target.value.length){
+      selectionStart = e.target.value.length;
+    }
+    if (selectionEnd > e.target.value.length){
+      selectionEnd = e.target.value.length;
+    }
+    if (selectionStart < 0 ){
+      selectionStart = 0;
+    }
+    if (selectionEnd < 0 ){
+      selectionEnd = 0;
+    }
+    this.updateCursorPos(selectionStart);
+    e.target.selectionStart = selectionStart;
+    e.target.selectionEnd = selectionEnd;
   }
-  updateCursorPos(e){
+  updateCursorPos(pos) {
     this.cursorBlink('stop');
-    if (e.target.selectionStart <= 21){
-      this.horzPosition = e.target.selectionStart - 21;
-    } else if (e.target.selectionStart > 21){
-      this.horzPosition = 0;
+    if (0 <= pos && pos <= this.width) {
+      this.cursorPosition = pos - this.width;
+    } else if (pos > this.width) {
+      this.cursorPosition = 0;
     }
     this.cursorBlink('start');
   }
-  onClick(e){
-    this.updateCursorPos(e);
+  onClick(e) {
+    this.updateCursorPos(e.target.selectionStart);
     document.querySelector('#input').firstChild.focus();
   }
-  focus(isFocused: boolean){
+  focus(isFocused: boolean) {
     const cursor = document.querySelector('#cursor') as HTMLElement;
-    if (!isFocused){
+    if (!isFocused) {
       this.cursorBlink('stop');
     }
   }
