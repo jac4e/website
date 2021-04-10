eb0941f2708b8e962d3801f538daa85ca0251686
Jacques Fourie <jacques@fourie.ca>
2021-04-10 15:28:33 -0700

    removed sticky header code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
-import { StickyHeaderDirective } from './sticky-header.directive';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
@@ -16,7 +15,6 @@ import { ErrorComponent } from './error/error.component';
   declarations: [
     AppComponent,
     InputComponent,
-    StickyHeaderDirective,
     ContactComponent,
     AboutComponent,
     ResumeComponent,
